800808b5c46f8b367057bf95a18208e7bed98d99
Hasan Alper Duzgun <alper-mf@hotmail.com>
2022-10-26 21:43:23 +0300

    feat(code): code is refactoring

--- bin/socket_server.dart
+++ bin/socket_server.dart
@@ -6,7 +6,7 @@ import 'package:socket_server/terminal_service.dart';
 Future<void> main() async {
   final InternetAddress ip = InternetAddress.anyIPv4;
   final ServerSocket server = await ServerSocket.bind(ip, 3000);
-  print('Server is running on ${ip.address}:${server.port}');
+  printGreen('Server is running on ${ip.address}:${server.port}');
   server.listen((Socket event) {
     handleConnection(event);
   });
